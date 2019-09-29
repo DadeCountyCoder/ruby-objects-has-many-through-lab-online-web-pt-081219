2d8838675058f0d958d49cd827be0b209cc00385
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-29 13:28:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,26 @@
-class Artist 
+class Artist
   attr_accessor :name
-  @@all=[]
-  
+
+  @@all = []
+
   def initialize(name)
-    @name=name
-    @@all<<self
+    @name = name
+    @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
-  def songs 
-   @songs=songs
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
   end
-  
-  # def songs
-  #   songs.all.collect do |artist|
-  # end
-  
-  
-  # def songs(name, genre)
-  #   songs.new(name,self,genre)
-  # end
 
-  # def songs
-  #   Song.all.select { |song| song.artist == self }
-  # end
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
 
-  # def genres
-  #   songs.map(&:genre)
-  # end
+  def genres
+    songs.map(&:genre)
+  end
 end

46e03629d5bf31929f29fc00838c081bbc49702d
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-29 13:28:41 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,26 @@
-# class Genre
-#   attr_accessor :name
+class Genre
+  attr_accessor :name
 
-#   @@all = []
+  @@all = []
 
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-# end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
+  end
+end
